c6f666dbc909158a81dad092b7027b232a5c5b68
nanami <h_daniel_min@yahoo.co.jp>
2022-06-12 21:29:53 +0900

    js

---
+++ mordal.js
@@ -0,0 +1,13 @@
+// モーダルが複数ある場合
+ $(function () {
+   // モーダルのボタンをクリックした時
+   $('.modal_trigger .modal_btn').on('click', function() {
+     var btnIndex = $(this).index(); // 何番目のモーダルボタンかを取得
+     $('.modal_area .modal_box').eq(btnIndex).fadeIn(); // クリックしたモーダルボタンと同じ番目のモーダルを表示する
+   });
+
+   // ×やモーダルの背景をクリックした時
+   $('.modal_close , .modal_bg').click(function(){
+     $('.modal_box').fadeOut(); // モーダルを非表示にする
+   });
+ });
